b5aaba5a7d4e52e5cac0fb8248e355c1798e0c72
James Okolie <zebedee788@gmail.com>
2023-05-19 00:59:14 +0400

    Creating table and Inserting data

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const mysql = require('mysql')
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '' //change this toany password immediatley
+    password: '', //change this to any password immediatley
+    database: 'nodemysql' //add this line after creating datatbase, before creating table
 })
 
 //connect to MySQL
@@ -19,7 +20,6 @@ db.connect(err => {
 const app = express()
 
 //Create Database
-
 app.get('/createdb', (req, res) => {
     let sql = 'CREATE DATABASE nodemysql'
     db.query(sql, err => {
@@ -28,4 +28,33 @@ app.get('/createdb', (req, res) => {
         }
         res.send('Database Created')
     })
+})
+
+//Create Table
+app.get('/createemployee', (req, res) => {
+    let sql = 'CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(255), designation VARCHAR(255), PRIMARY KEY(id))'
+    db.query(sql, err => {
+        if(err) {
+            throw err
+        }
+        res.send('Employee table created')
+    })
+})
+
+//Insert employee
+app.get('/employee1', (req, res) => {
+    let post = {name: 'Jake smith', designation: 'Cheif Executive Oficcer'}
+    let sql = 'INSERT INTO employee SET ?'
+    let query = db.query(sql, post, err => {
+        if(err) {
+            throw err
+        }
+        res.send('Employee added')
+    })
+})
+
+
+// 3000 is always the default port
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
 })
